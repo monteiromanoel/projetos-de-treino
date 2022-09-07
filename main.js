5d791255a61992c67808f6393af8a4e04549a5fb
Manoel Monteiro <69632699+monteiromanoel@users.noreply.github.com>
2022-09-06 22:18:04 -0300

    some updates

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function convertInc(value) {
     document.getElementById("answer-space1").innerHTML = value * 2.54
 }
+function convertFoot(value) {
+    document.getElementById("answer-space2").innerHTML = (value / 3.281).toFixed(4);
+    
+}
